cf2595c515bc9f6302e373c2384059e47c6ab275
minoro75 <minoro75@gmail.com>
2022-01-28 13:19:59 +0200

    1-[Tech]- MainActivity.kt - refactor setContent{}

--- app/src/main/java/io/minoro75/composecodelab/MainActivity.kt
+++ app/src/main/java/io/minoro75/composecodelab/MainActivity.kt
@@ -3,13 +3,14 @@ package io.minoro75.composecodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import io.minoro75.composecodelab.ui.theme.ComposeCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,21 +18,24 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeCodelabTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                MyApp()
             }
         }
     }
 }
 
 @Composable
+private fun MyApp() {
+    Surface(color = MaterialTheme.colors.surface) {
+        Greeting(name = "Android")
+    }
+}
+
+@Composable
 fun Greeting(name: String) {
     Surface(color = MaterialTheme.colors.surface) {
-        Text(text = "Hello $name!")
+        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
     }
-
 }
 
 @Preview(showBackground = true)
